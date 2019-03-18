bb92bc411e1b1146246cb6b491896d01f46754c4
sinumohammed <sinumohammed@gmail.com>
2019-03-18 08:23:54 +0530

    conditional renderings

--- my-app/src/components/counter.jsx
+++ my-app/src/components/counter.jsx
@@ -5,28 +5,24 @@ class Counter extends Component {
     count: 1,
     tags: ["tag1", "tag2", "tag3"]
   };
+  rendrTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
 
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
   render() {
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.rendrTags()}
       </div>
     );
-  }
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
   }
 }
 
