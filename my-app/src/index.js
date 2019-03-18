14ce71e2af56f50dbef6022fb60e05abb8c896d5
sinumohammed <sinumohammed@gmail.com>
2019-03-18 07:40:38 +0530

    My first component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter";
 
-const element = <h1>Hello World</h1>;
+ReactDOM.render(<Counter />, document.getElementById("root"));
 
-console.log(element);
-
-ReactDom.render(element, document.getElementById("root"));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
